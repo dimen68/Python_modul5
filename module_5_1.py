0f726bc49ea10e90ad91f5330ea92656c3c103a6
bochar68 <bochar.dg@gmail.com>
2024-11-03 12:07:18 +0300

    Добавлена задержка при перемещении по этажам

--- module_5_1.py
+++ module_5_1.py
@@ -1,16 +1,19 @@
 # Задача "Developer - не только разработчик"
+from time import sleep
+
 
 class House:
     def __init__(self, name, floors):
         self.name = name
         self.number_of_floors = floors
 
-
     def go_to(self, new_floor):
-        if  1 > new_floor or new_floor > self.number_of_floors:
-            print(f'\n Такого этажа ({new_floor}) не существует, так как в доме "{self.name}" всего {self.number_of_floors} этажей.')
+        if 1 > new_floor or new_floor > self.number_of_floors:
+            print(
+                f'\n Такого этажа ({new_floor}) не существует, так как в доме "{self.name}" всего {self.number_of_floors} этажей.')
         else:
             for i in range(1, new_floor + 1):
+                sleep(1)
                 print(f'этаж {i}')
 
 
